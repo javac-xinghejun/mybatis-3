319f3725b22c0eb42d260e3ff5448e0097183178
xinghe <xinghejun@foxmail.com>
2023-01-03 09:25:59 +0800

    添加JDBCTest

--- src/test/java/org/apache/ibatis/xinghe/jdbc/JDBCTest.java
+++ src/test/java/org/apache/ibatis/xinghe/jdbc/JDBCTest.java
@@ -1,14 +1,11 @@
 package org.apache.ibatis.xinghe.jdbc;
 
-import com.mysql.cj.jdbc.Driver;
-
 import java.sql.*;
 
 public class JDBCTest {
-    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/mybatis?useUnicode=&characterEncoding=UTF-8&serverTimezone=UTC", "root", "root123456");
-        Statement statement = connection.createStatement();
         String sql = "select * from mybatis_test where id = ?";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setLong(1, 1L);
